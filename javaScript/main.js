09036e6ad246585ea1107c0c8496451d542e3feb
PaulVieira <45637648+PaulVieira@users.noreply.github.com>
2021-02-01 18:42:36 -0300

    Add files via upload

---
+++ javaScript/main.js
@@ -0,0 +1,63 @@
+
+/*var nome = "Paulo Vieira";
+var idade = "33";
+var idade2 = "25"
+//alert("Seja bem vindo " + nome);
+alert (idade + idade2);*/ //concatena os valores de idade e idade2
+/*
+var nome = "Paulo Vieira";
+var idade = 33 
+var idade2 = 25
+var frase = "Japão é louco time"
+
+//alert (idade + idade2); // ler como numeros interiros a serem somados
+
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("japão", "Brazil"));
+alert(frase.replace("japão", "Brasil"));*/
+
+/*=============================================================
+lista de Array
+*/
+
+/*var lista = ["maça", "Pêra", "Laranja", "Manga" ];
+lista.push("uva", "abacate", "Melancia");//add element to lista
+//lista.pop()// retira o ultimo element da lista
+//console.log(lista.length);//"length" mostra Nª elementos na lista
+//console.log(lista.reverse()); "reverse" passa a lista de forma reversa - ultimo como primeiro
+console.log(lista);
+console.log(lista.toString());//perde o efeito de array - vira string
+console.log(lista.join(" - "));   // separação entre elementos*/
+
+//Dicionário
+
+/*var fruta = {nome:"maça", Cor:"Vermelha"};
+console.log(fruta)*/
+
+/*===================================================================================
+Condicional
+*/
+
+/*var idade = prompt("Informe Qual a sua idade?")
+    if (idade >= 18){
+        alert("Maior de idade");
+    }else{
+        alert("Menor de idade");
+    }*/
+
+/*===================================================================================
+Repetição
+*/
+
+/*var count = 0;
+
+while (count < 10){
+    console.log(count);
+    count++;
+}*/
+
+var count;
+    for (count = 0; count <= 15; count++){
+        console.log(count)
+    }
